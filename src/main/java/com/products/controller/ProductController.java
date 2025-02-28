0d5893a136076456c28dcf6be7403216121f5bae
Prakash <punj.prakash@aa.com>
2025-02-28 13:29:57 -0600

    Added files for

--- src/main/java/com/products/controller/ProductController.java
+++ src/main/java/com/products/controller/ProductController.java
@@ -6,11 +6,10 @@ package com.products.controller;
 import com.products.data.Product;
 import com.products.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 import java.util.Optional;
@@ -21,7 +20,7 @@ public class ProductController {
     @Autowired
     private ProductService productService;
 
-    @GetMapping("/api/{productId}")
+    @GetMapping("/service/{productId}")
     public ResponseEntity<Product> getProductById(@PathVariable int productId) {
         Optional<Product> product = productService.getProductById(productId);
         return product.map(ResponseEntity::ok)
@@ -30,9 +29,24 @@ public class ProductController {
 
     //Allow all ogi
     //@CrossOrigin(origins = "*") // Allow only this origin
-    @GetMapping("/api/products")
+    @GetMapping("/service/newproducts")
     public ResponseEntity<List<Product>> getProducts() {
         List<Product> products = productService.getProducts();
         return ResponseEntity.ok(products);
     }
+
+    @GetMapping("/proxy")
+    public ResponseEntity<String> proxyRequest(@RequestParam String url) {
+        RestTemplate restTemplate = new RestTemplate();
+
+        try {
+            ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
+            return new ResponseEntity<>(response.getBody(), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Error: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+
+
 }
